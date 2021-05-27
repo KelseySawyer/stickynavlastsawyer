50448e8eb6be8677ccb6a9ab37cf4f584beab43d
KelseySawyer <77423771+KelseySawyer@users.noreply.github.com>
2021-05-27 16:59:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+const nav=document.querySelectorAll('.nav')
+window.addEventListener('scroll', fixNav)
+
+
+function fixNav(){
+	if(window.scrollY>nav.offsetHeight + 150){
+		nav.classList.add('active')
+	}else{
+		nav.classList.remove9('active')
+	}
+	
+}
